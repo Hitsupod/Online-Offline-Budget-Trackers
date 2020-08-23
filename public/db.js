31b6515e213bef4d35085730b3399abe44daf1e0
Reggie Littleton <Hitsupod@gmail.com>
2020-08-22 22:17:36 -0500

    Services workers creation

--- public/db.js
+++ public/db.js
@@ -1,15 +1,17 @@
+const { response } = require("express");
+
 let db;
 // Creating the database
 const request = indexDB.open("budget", 1);
 
 // Create the object store
-request.onupgradeneeded = (event) => {
+request.onupgradeneeded = function (event) {
     const db = event.target.reesult;
     db.createObjectStore("pending", {autoIncrement: true});
 };
 
 // Check online 
-request.onsuccess = (event) => {
+request.onsuccess = function (event) {
     db = event.target.result;
     if (navigator.onLine) {
         checkDataBase();
@@ -17,6 +19,46 @@ request.onsuccess = (event) => {
 };
 
 // Log Error
-request.onerror = (event) => {
+request.onerror = function (event) {
     console.log(event.target.errorCode)
 };
+
+// Store Record
+function storeRecord(record) {
+    // Creating transaction on pending db
+    const transaction = db.transaction(["pending"], "readwrite");
+    // Accessing the store
+    const store = transaction.objectStore("pending");
+    //Adding the record
+    store.add(record);
+}
+
+function checkDataBase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    // Get and store all Records from store
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if(getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                // Clear the store
+                store.clear();
+            })
+        }
+    }
+}
+
+// Listen for back online 
+window.addEventListener("online", checkDataBase);
